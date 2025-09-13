a845ed8de56d883ebb0ac7b4cd334485800de2a6
abrham alemu <ergetie14@gmail.com>
2025-09-13 11:42:24 +0300

    deploy vite

--- netflix-clone/vite.config.js
+++ netflix-clone/vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/Netflix-Clone-2025-react.js/", // 👈 IMPORTANT: must match your repo name
+});
